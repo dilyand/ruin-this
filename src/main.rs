2dd8a9e62e9c68b7762a97c0359194c4cff55ae3
Dilyan Damyanov <dilyand@gmail.com>
2020-05-23 23:30:54 +0100

    Add word variations

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::io::{BufRead, BufReader, Result};
 
 fn main() {
-    println!("Give me a string and I'll tell you if it's a word.");
+    println!("Give me a word and I'll tell you similar words.");
 
     let mut input = String::new();
 
@@ -11,15 +11,13 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    if is_word(&input.trim().to_lowercase()) == true {
-        println!("{} is a word", input.trim())
-    } else {
-        println!("{} is not a word", input.trim())
-    };
+    let vars = word_variations(&input.to_lowercase().trim());
+
+    println!("Here are some variations: {:?}", vars);
 }
 
 fn is_word(w: &str) -> bool {
-    let file = File::open("dict/words_alpha.txt").expect("Cannot open file.");
+    let file = File::open("dict/english3.txt").expect("Cannot open file.");
     let reader = BufReader::new(file);
 
     let result: Vec<_> = reader
@@ -29,4 +27,29 @@ fn is_word(w: &str) -> bool {
         .collect();
 
     return !result.is_empty();
+}
+
+fn word_variations(w: &str) -> Vec<String> {
+    const ASCII_LOWER: [char; 26] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+
+    let mut v: Vec<String> = Vec::new();
+
+    for i in 0..w.len() {
+        for l in &ASCII_LOWER {
+            let mut test_word_char: Vec<char> = w.chars().collect();
+            if test_word_char[i] != *l {
+                test_word_char[i] = *l;
+                let s_test_word: String = test_word_char.into_iter().collect();
+                let test_word: &str = &*s_test_word;
+                if is_word(test_word) == true {
+                    v.push((*test_word).to_owned())
+                }
+            }
+        }
+    }
+
+    return v;
 }
